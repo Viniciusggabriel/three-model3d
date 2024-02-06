b44b84e318a302335bab6e99783d9046c7325df9
Vinícius Gabriel <viniciusggabriel989@protonmail.com>
2024-02-06 01:26:43 -0300

    Adiciona adaptadores SSR: Ira adicionar o adptador da vercel
    - Modificara o Packge-lock e packge json com as dependências do  adaptadores SSR
    - Modificação no astro.config.mjs

--- portfolio-app/astro.config.mjs
+++ portfolio-app/astro.config.mjs
@@ -1,8 +1,14 @@
+import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
+  output: "server",
+  adapter: vercel(),
+  imagesConfig: {
+    sizes: [320, 640, 1280],
+  },
 });
